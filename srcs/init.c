1bb09caad5510ca284d9fadfa1eabc93346413c5
Tanguy COPPIN <tcoppin@student.42.fr>
2015-05-03 16:21:19 +0200

    init all

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: tcoppin <tcoppin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/02 06:41:08 by jripoute          #+#    #+#             */
-/*   Updated: 2015/05/03 16:15:44 by tcoppin          ###   ########.fr       */
+/*   Updated: 2015/05/03 16:20:38 by tcoppin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,8 @@ static void			init_ball(t_ball *ball)
 
 void			init_all(t_all *all)
 {
+	all->lv = NULL;
+	all->cur = 0;
 	parse_lvl(all, "./levels/");
 	init_ship(&all->ship);
 	init_ball(&all->ball);
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tcoppin <tcoppin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/03 13:31:02 by tcoppin           #+#    #+#             */
-/*   Updated: 2015/05/03 16:12:03 by tcoppin          ###   ########.fr       */
+/*   Updated: 2015/05/03 16:20:13 by tcoppin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ int		main(void)
 {
 	t_all	all;
 
-	all.lv = NULL;
-	all.cur = 0;
 	init_all(&all);
 	while (!glfwWindowShouldClose(all.win.ptr))
 		content_win(&all);
